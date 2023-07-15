90e95960302e65202d7a70fe2f4d9503d74e370d
unknown <developer.ramanpreetsingh@gmail.com>
2023-07-15 11:00:48 +0530

    change backend url

--- FrontEnd/src/utils/constants.jsx
+++ FrontEnd/src/utils/constants.jsx
@@ -1,6 +1,6 @@
 let constant = {
     BACKEND_URL_LOCAL: 'http://localhost:3000/',
-    BACKEND_URL_ONLINE: 'https://rps-sh-tinyurl-backend.vercel.app',
+    BACKEND_URL_ONLINE: 'https://rps-sh-tinyurl-backend.vercel.app/',
     NEW_REQUEST: "newrequest",
     SIGNUP: "signup",
     SIGNIN: "signin",
